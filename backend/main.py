c77fb26a6090f681278d4f95859a7f76a5217edb
bbrunossf <brunossf4@gmail.com>
2026-06-04 22:16:54 -0300

    adicionado parametro remote_components no yt_opts

--- backend/main.py
+++ backend/main.py
@@ -83,7 +83,7 @@ def search_playlists(query):
     print("executando a busca por playlists...")
     response = request.execute()
     #print(response.keys()) #retorna o 'items'
-    
+
     #retorna a url da playlist, o título e o ID
     #print(f"response: {response}") #para verificar o que está sendo retornado
     return response
@@ -103,15 +103,15 @@ def get_videos_info(playlist_id):
         maxResults=50  # Pega até 50 vídeos (ajuste conforme necessário)
     )
     response = request.execute()
-    #print(f"{playlist_id} - {response}") 
+    #print(f"{playlist_id} - {response}")
 
     # Extrai informações dos vídeos na playlist
     for item in response['items']:
         if item['snippet']['resourceId'].get('kind') != "youtube#video":
             continue
         video_title = item['snippet']['title']
-        video_id = item['snippet']['resourceId']['videoId']        
-        
+        video_id = item['snippet']['resourceId']['videoId']
+
         # Tenta obter o URL do thumbnail, tratando o caso de ausência
         try:
             video_thumbnail = item['snippet']['thumbnails']['standard']['url']
@@ -153,13 +153,13 @@ async def search_tracks(request: SearchRequest):
     #print(f"dados de playlist retornados: {response}") #retorna um youtube#searchListResponse, composto por
     #'items', que é uma lista de dicionários youtube#searchResult que tem 'kind', 'id' e 'snippet', e cada 'snippet' tem:
     #'title' , que é o título da playlist
-    
+
     if not response.get('items'):
         raise HTTPException(
             status_code=404,
             detail="Nenhuma playlist encontrada para esta música"
         )
-           
+
     # playlists = [
     # {
     #     "title": item['snippet']['title'],
@@ -167,11 +167,11 @@ async def search_tracks(request: SearchRequest):
     # }
     # for item in response['items']
     # if item['id'].get('kind') == "youtube#playlist"
-    # ]   
+    # ]
 
     #print(f"playlists encontradas: {playlists}") #retorna uma lista de dicionários com 'title' e 'id' da playlist
     playlists = []
-    
+
     for item in response['items']:
         if item['id'].get('kind') != "youtube#playlist":
             continue
@@ -179,15 +179,15 @@ async def search_tracks(request: SearchRequest):
         playlist_id = item['id']['playlistId']
         #print(f"playlist_id: {playlist_id}") #para verificar se o ID da playlist está correto
         playlist_title = item['snippet']['title']
-        
+
         # Obter vídeos relacionados à playlist
         videos = get_videos_info(playlist_id)
-        
+
         playlists.append(Playlist(id=playlist_id, title=playlist_title, videos=videos))
 
     #return {"playlists": playlists}
-    return SearchResponse(playlists=playlists)    
-    
+    return SearchResponse(playlists=playlists)
+
 
 
 
@@ -222,13 +222,14 @@ async def import_playlist(url: str = Form(...)):
 
 def download_video(video_id: str):
     ydl_opts = {
+        "remote_components": "ejs:github", #agora precisa
         'format': 'best',  # Define que quer baixar o melhor formato de vídeo #VOU DEIXAR O DEFAULT MESMO
         'outtmpl': 'downloads/videos/%(title)s.%(ext)s',  # Define o template do nome do arquivo
-        
+
         'addmetadata': True,
         'embedmetadata': True,
         'writethumbnail': True,
-        
+
         'postprocessors': [
         {
             'key': 'FFmpegMetadata',
@@ -245,16 +246,16 @@ def download_audio(video_id: str):
     ydl_opts = {
         'format': 'bestaudio/best',
         'outtmpl': 'downloads/musicas/%(artist,creator)s/%(title)s.%(ext)s',
-        
+
         'addmetadata': True,
         'embedmetadata': True,
         'writethumbnail': True,
         'embedthumbnail': True,
-        
+
         'parse_metadata': [
             'title:%(artist)s - %(title)s'
         ],
-        
+
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
@@ -263,7 +264,7 @@ def download_audio(video_id: str):
         {'key': 'EmbedThumbnail'},
         {'key': 'FFmpegMetadata'},
         ],
-    }    
+    }
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             ydl.download([f'https://www.youtube.com/watch?v={video_id}'])
@@ -327,4 +328,4 @@ def get_download_status(job_id: str):
     if job_id not in jobs:
         raise HTTPException(status_code=404, detail="Job não encontrado")
 
-    return jobs[job_id]    
+    return jobs[job_id]
